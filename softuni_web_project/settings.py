43addb21d348d0a1b86cef84dc6522bd871a6aa1
Ivan <brain.games@abv.bg>
2022-04-10 12:48:05 +0300

    static

--- softuni_web_project/settings.py
+++ softuni_web_project/settings.py
@@ -131,10 +131,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_URL = 'staticfiles/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    BASE_DIR / 'staticfiles',
+    BASE_DIR / 'static',
 )
 MEDIA_ROOT = BASE_DIR / 'mediafiles'
 MEDIA_URL = '/media/'
