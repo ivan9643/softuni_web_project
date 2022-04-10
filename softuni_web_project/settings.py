1d74a31911c4110a9fea3cef59873de1b892cf12
Ivan <brain.games@abv.bg>
2022-04-10 14:04:27 +0300

    added migrations

--- softuni_web_project/settings.py
+++ softuni_web_project/settings.py
@@ -99,7 +99,9 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+import dj_database_url
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
