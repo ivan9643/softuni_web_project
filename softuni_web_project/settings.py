8e7dfe959537079265ee5fc1642da90f50dcb9ed
Ivan <brain.games@abv.bg>
2022-04-15 23:57:52 +0300

    settings

--- softuni_web_project/settings.py
+++ softuni_web_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-54@)d!x$3zfpr8tzwr+t)9^v3&o=+ub9gzm26$2rzis+up^@lb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
