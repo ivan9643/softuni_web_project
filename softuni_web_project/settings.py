ab00f0859f87433139bdfdae4c6bfe397bf3475c
Ivan <brain.games@abv.bg>
2022-04-15 23:44:20 +0300

    settings

--- softuni_web_project/settings.py
+++ softuni_web_project/settings.py
@@ -100,10 +100,10 @@ DATABASES = {
     }
 }
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
+# db_from_env = dj_database_url.config(conn_max_age=600)
 # DATABASES['default'].update(db_from_env)
 
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default'] = dj_database_url.config(conn_max_age=None)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
